57c331876b4388e6765b80d2cd227122bf0b18fd
vadimkononenko <vadiskon@gmail.com>
2019-11-04 15:25:33 +0200

    Bugfix: renamed methods according to conversation of JUnit

--- src/test/java/com/itea/homeworks/CalculationServiceTest.java
+++ src/test/java/com/itea/homeworks/CalculationServiceTest.java
@@ -4,83 +4,99 @@ import com.itea.homeworks.utils.calculation.Service;
 import org.junit.Before;
 import org.junit.Test;
 
+import static jdk.nashorn.internal.objects.Global.Infinity;
 import static org.junit.Assert.assertEquals;
 
 public class CalculationServiceTest {
     private Service service;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp(){
         service = new Service();
     }
 
     @Test
-    public void add(){
+    public void shouldAddDigits() {
         double expected = 10.0;
         double delta = 0;
 
-        double actual = service.calculate(2,8,'+');
+        double actual = service.calculate(2, 8, '+');
 
-        assertEquals(expected,actual,delta);
+        assertEquals(expected, actual, delta);
     }
 
     @Test
-    public void subtract(){
+    public void shouldSubtractDigits() {
         double expected = 5;
         double delta = 0;
 
-        double actual = service.calculate(10,5,'-');
+        double actual = service.calculate(10, 5, '-');
 
-        assertEquals(expected,actual,delta);
+        assertEquals(expected, actual, delta);
     }
 
     @Test
-    public void multiply(){
+    public void shouldMultiplyDigits() {
         double expected = 25;
         double delta = 0;
 
-        double actual = service.calculate(5,5,'*');
+        double actual = service.calculate(5, 5, '*');
 
-        assertEquals(expected,actual,delta);
+        assertEquals(expected, actual, delta);
     }
 
     @Test
-    public void divide(){
-        double expected = 5;
+    public void shouldDivideDigits() {
+        double expected1 = 5;
+        double expected2 = Infinity;
         double delta = 0;
 
-        double actual = service.calculate(25,5,'/');
+        double actual1 = service.calculate(25, 5, '/');
+        double actual2 = service.calculate(25, 0, '/');
 
-        assertEquals(expected,actual,delta);
+        assertEquals(expected1, actual1, delta);
+        assertEquals(expected2, actual2, delta);
     }
 
     @Test
-    public void square(){
-        double expected = 1.379729661461215;
+    public void shouldSquareDigits() {
+        double expected1 = 1.379729661461215;
+        double expected2 = Infinity;
+        double expected3 = 0;
         double delta = 0;
 
-        double actual = service.calculate(5,5,'√');
+        double actual1 = service.calculate(5, 5, '√');
+        double actual2 = service.calculate(5, 0, '√');
+        double actual3 = service.calculate(0, 5, '√');
 
-        assertEquals(expected,actual,delta);
+        assertEquals(expected1, actual1, delta);
+        assertEquals(expected2, actual2, delta);
+        assertEquals(expected3, actual3, delta);
     }
 
     @Test
-    public void pow(){
-        double expected = 3125;
+    public void shouldPowDigits() {
+        double expected1 = 3125;
+        double expected2 = 1;
+        double expected3 = Infinity;
         double delta = 0;
 
-        double actual = service.calculate(5,5,'^');
+        double actual1 = service.calculate(5, 5, '^');
+        double actual2 = service.calculate(5, 0, '^');
+        double actual3 = service.calculate(5, Infinity, '^');
 
-        assertEquals(expected,actual,delta);
+        assertEquals(expected1, actual1, delta);
+        assertEquals(expected2, actual2, delta);
+        assertEquals(expected3, actual3, delta);
     }
 
     @Test
-    public void powAdvanced(){
+    public void shouldPowAdvancedDigits() {
         double expected = 14161;
         double delta = 0;
 
-        double actual = service.calculate(5,5,'~');
+        double actual = service.calculate(5, 5, '~');
 
-        assertEquals(expected,actual,delta);
+        assertEquals(expected, actual, delta);
     }
 }
