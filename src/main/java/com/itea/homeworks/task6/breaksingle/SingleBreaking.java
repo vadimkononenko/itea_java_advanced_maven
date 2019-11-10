e206c39b6c18bae71e339c8963420e0485dba54f
vadimkononenko <vadiskon@gmail.com>
2019-11-10 11:35:49 +0200

    Bugfix: changed IllegalAccessException, InvocationTargetException, InstantiationException -> Exception, instance1,2 -> singleton1,2

--- src/main/java/com/itea/homeworks/task6/breaksingle/SingleBreaking.java
+++ src/main/java/com/itea/homeworks/task6/breaksingle/SingleBreaking.java
@@ -3,21 +3,20 @@ package com.itea.homeworks.task6.breaksingle;
 import com.itea.homeworks.task5.singleton.Singleton;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 public class SingleBreaking {
-    public Singleton breakSingle(Singleton instance1) throws IllegalAccessException, InvocationTargetException, InstantiationException {
-        Singleton instance2 = instance1.getInstance();
+    public Singleton breakSingle(Singleton instance1) throws Exception {
+        Singleton singleton2 = instance1.getInstance();
 
         Constructor[] constructors = Singleton.class
                 .getDeclaredConstructors();
 
         for(Constructor constructor : constructors){
             constructor.setAccessible(true);
-            instance2 = (Singleton) constructor.newInstance();
+            singleton2 = (Singleton) constructor.newInstance();
             break;
         }
 
-        return instance2;
+        return singleton2;
     }
 }
