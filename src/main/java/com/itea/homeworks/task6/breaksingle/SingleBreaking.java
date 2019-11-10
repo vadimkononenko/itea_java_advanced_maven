f29d235e2b23bea5a0dc2ce28aaea92dcc62d1d6
vadimkononenko <vadiskon@gmail.com>
2019-11-10 11:38:14 +0200

    Bugfix: changed moved line

--- src/main/java/com/itea/homeworks/task6/breaksingle/SingleBreaking.java
+++ src/main/java/com/itea/homeworks/task6/breaksingle/SingleBreaking.java
@@ -8,8 +8,7 @@ public class SingleBreaking {
     public Singleton breakSingle(Singleton singleton1) throws Exception {
         Singleton singleton2 = singleton1.getInstance();
 
-        Constructor[] constructors = Singleton.class
-                .getDeclaredConstructors();
+        Constructor[] constructors = Singleton.class.getDeclaredConstructors();
 
         for(Constructor constructor : constructors){
             constructor.setAccessible(true);
